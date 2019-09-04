17a91eb191ef8e263b1762a8094eaf70e5f04a30
mrfearless <fearless.is.my.name@gmail.com>
2019-09-04 18:53:40 +0100

    v1.0.0.16 - revert adding lua libs (io, os and package) by linking static lua library

--- EEexDLL/EEexLua.asm
+++ EEexDLL/EEexLua.asm
@@ -106,16 +106,6 @@ EEexLuaInit PROC C lua_State:DWORD, lpszString:DWORD
         ret
     .ENDIF    
     
-    ; 04/09/2019 - add in other lua libraries
-    Invoke luaL_requiref, g_lua, CTEXT("io"), Addr luaopen_io, 1
-    Invoke lua_settop, g_lua, -2
-    
-    Invoke luaL_requiref, g_lua, CTEXT("os"), Addr luaopen_os, 1
-    Invoke lua_settop, g_lua, -2
-    
-    Invoke luaL_requiref, g_lua, CTEXT("package"), Addr luaopen_package, 1
-    Invoke lua_settop, g_lua, -2
-    
     ;---------------------------
     ; For prototype of no params
     ;---------------------------
